fdccc6be36e35ff68083401ce6128975bcab049e
OanaRaluca <oana.petre14@yahoo.com>
2015-08-20 16:36:54 +0300

    Create content-image.php

---
+++ content-image.php
@@ -0,0 +1,25 @@
+<?php
+
+
+
+tha_entry_before(); ?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php tha_entry_top(); ?>
+	
+	<div class="entry-content clearfix">
+		<?php
+		the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'the-bootstrap' ) );
+		the_bootstrap_link_pages(); ?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php the_bootstrap_posted_on(); ?>
+	</footer><!-- .entry-footer -->
+	
+	<?php tha_entry_bottom(); ?>
+</article><!-- #post-<?php the_ID(); ?> -->
+<?php tha_entry_after();
+
+
+/* End of file content-image.php */
+/* Location: ./wp-content/themes/the-bootstrap/partials/content-image.php */
