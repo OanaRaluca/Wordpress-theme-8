40a5b141ec212abd721651b42214935b7adcf8e1
OanaRaluca <oana.petre14@yahoo.com>
2015-08-20 16:40:39 +0300

    Create _full_width.php

---
+++ _full_width.php
@@ -0,0 +1,24 @@
+<?php
+
+get_header(); ?>
+
+<section id="primary" class="span12">
+	<?php tha_content_before(); ?>
+	<div id="content" role="main">
+		<?php tha_content_top();
+		
+		the_post();
+		get_template_part( '/partials/content', 'page' );
+		comments_template( '', true );
+		
+		tha_content_bottom(); ?>
+	</div><!-- #content -->
+	<?php tha_content_after(); ?>
+</section><!-- #primary -->
+
+<?php
+get_footer();
+
+
+/* End of file _full_width.php */
+/* Location: ./wp-content/themes/the-bootstrap/_full_width.php */
